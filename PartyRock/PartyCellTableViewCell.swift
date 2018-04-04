8dbe0129715d966f9844dd8ae202adba82570f3e
Brandon Rodriguez <brandonart1224@gmail.com>
2018-04-03 22:24:54 -0700

    first commit

---
+++ PartyRock/PartyCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyCellTableViewCell.swift
+//  PartyRock
+//
+//  Created by BRANDON RODRIGUEZ on 8/15/17.
+//  Copyright © 2017 BRANDON RODRIGUEZ. All rights reserved.
+//
+
+import UIKit
+
+class PartyCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PartyRock/PartyRock.swift
@@ -0,0 +1,9 @@
+//
+//  PartyRock.swift
+//  PartyRock
+//
+//  Created by BRANDON RODRIGUEZ on 8/15/17.
+//  Copyright © 2017 BRANDON RODRIGUEZ. All rights reserved.
+//
+
+import Foundation
